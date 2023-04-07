c71f096c19c56cb7101dbf7585a446442624d5fb
rahul1387 <rahultripathi7447@gmail.com>
2023-04-07 12:58:53 -0700

    Loops Question Solved

---
+++ src/PracticeSet5.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class PracticeSet5 {
+    public static void main(String[] args) {
+
+        //Question1
+//        int n=4;
+//        for (int i=n; i>0;i--){
+//            for (int j=0; j<i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.print("\n");
+//        }
+
+        //Question2
+
+        Scanner sc= new Scanner (System.in);
+//        int sum = 0;
+//        int exit =1;
+//
+//        while (exit==1) {
+//            System.out.print("Enter even number1: ");
+//            int n1 = sc.nextInt();
+//            System.out.print("Enter even number2: ");
+//            int n2 = sc.nextInt();
+//            System.out.print("Enter even number3: ");
+//            int n3 = sc.nextInt();
+//            int n = n1 + n2 + n3;
+//            for (int i = 0; i < n; i++) {
+//                sum = sum + (2 * i);
+//            }
+//            System.out.print("The sum of the even number is: ");
+//            System.out.println(n);
+//            System.out.println("If you want to continue (press1) if not (press 0)");
+//            exit =sc.nextInt();
+//        }
+
+    }
+}
+
+
